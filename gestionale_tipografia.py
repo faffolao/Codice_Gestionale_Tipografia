2bd537449aff3e4943e817d55bc0540e79f03c5f
Federico Arduini <s1098699@studenti.univpm.it>
2022-11-29 10:29:41 +0100

    inserimento delle prime cose

--- gestionale_tipografia.py
+++ gestionale_tipografia.py
@@ -1,1 +1,13 @@
-ok
+from PyQt5 import QtWidgets, uic
+import sys
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('ui/miao.ui', self)
+        self.show()
+        self.setFixedSize(self.size())
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
