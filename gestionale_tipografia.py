af1d57bc290cfa71e4226cbb3254f31ba5bc4f37
Federico Arduini <s1098699@studenti.univpm.it>
2022-11-29 11:12:42 +0100

    che palle

--- gestionale_tipografia.py
+++ gestionale_tipografia.py
@@ -1,12 +1,14 @@
 from PyQt5 import QtWidgets, uic
 import sys
 
+
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
-        uic.loadUi('ui/miao.ui', self)
+        uic.loadUi('ui/login_form.ui', self)
         self.show()
         self.setFixedSize(self.size())
+
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
